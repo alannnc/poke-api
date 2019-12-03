8e7e058a9d155ef26c8db7c30e0e5a8b5a17ec6c
Alan <alannnc@gmail.com>
2019-12-02 23:43:58 -0600

    Edited package json and config file

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
-const Dotenv = require('dotenv');
+const Dotenv = require("dotenv");
 
 Dotenv.config();
 
 const config = {
-  API_PORT: process.env.API_PORT,
-  BASE_URL: process.env.BASE_URL
+  API_PORT: process.env.API_PORT | 8000,
+  BASE_URL: process.env.BASE_URL | "https://pokeapi.co/api/v2/"
 };
 
 module.exports = config;
