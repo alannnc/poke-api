c8bc14f8a06f7a9c00f5976364361f787c6525d9
Alan <alannnc@gmail.com>
2019-12-03 00:59:58 -0600

    Fix on config environment variables

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const Koa = require('koa');
+const Koa = require("koa");
 
-const config = require('./src/config');
-const customRouter = require('./src/router');
-// const cors = require('@koa/cors');
+const config = require("./src/config");
+const customRouter = require("./src/router");
 
 const app = new Koa();
 
 app.use(customRouter);
-// app.use(cors);
-
 app.listen(config.API_PORT, () => {
   console.log(`Server listening on port: ${config.API_PORT}`);
 });
--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,11 @@
 const Dotenv = require("dotenv");
 
 Dotenv.config();
-
+const port = process.env.API_PORT || 3000;
+const baseUrl = process.env.BASE_URL || "127.0.0.1";
 const config = {
-  API_PORT: process.env.API_PORT | 8000,
-  BASE_URL: process.env.BASE_URL | "https://pokeapi.co/api/v2/"
+  API_PORT: port,
+  BASE_URL: baseUrl
 };
 
 module.exports = config;
